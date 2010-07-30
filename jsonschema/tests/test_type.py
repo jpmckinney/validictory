0ab44fe7533d5f54bfbcb186a1eb0a210be54d30
James Turk <james.p.turk@gmail.com>
2010-07-30 12:43:42 -0400

    test cleanup pt 2

--- jsonschema/tests/test_type.py
+++ jsonschema/tests/test_type.py
@@ -1,157 +1,103 @@
-#!/usr/bin/env python
-#:coding=utf-8:
-#:tabSize=2:indentSize=2:noTabs=true:
-#:folding=explicit:collapseFolds=1:
-
 from unittest import TestCase
 
 import jsonschema
 
 class TestType(TestCase):
-    
     def test_schema(self):
-      schema = { 
-        "type": [
-          { "type": "array",  "minItems": 10 },
-          { "type": "string", "pattern": "^0+$" } 
-        ]
-      }
-      
-      data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-      data2 = "0";
-      data3 = 1203;
-      
-      for x in [data1, data2]:
-        try:
-          jsonschema.validate(x, schema)
-        except ValueError, e:
-          self.fail("Unexpected failure: %s" % e)
-      
-      try:
-        jsonschema.validate(data3, schema)
-      except ValueError:
-        pass
-      else:
-        self.fail("Expected failure for %s" % repr(x))
-      
+        schema = {
+            "type": [
+                { "type": "array", "minItems": 10 },
+                { "type": "string", "pattern": "^0+$" }
+            ]
+        }
+
+        data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        data2 = "0"
+        data3 = 1203
+
+        for x in [data1, data2]:
+            try:
+                jsonschema.validate(x, schema)
+            except ValueError, e:
+                self.fail("Unexpected failure: %s" % e)
+
+        self.assertRaises(ValueError, jsonschema.validate, data3, schema)
+
+    def _test_type(self, typename, valids, invalids):
+        for x in valids:
+            try:
+                jsonschema.validate(x, {"type":typename})
+            except ValueError, e:
+                self.fail("Unexpected failure: %s" % e)
+
+        for x in invalids:
+            self.assertRaises(ValueError, jsonschema.validate, x,
+                              {"type":typename})
+
     def test_integer(self):
-      for x in [1, 89, 48, 32, 49, 42]:
-        try:
-          jsonschema.validate(x, {"type":"integer"})
-        except ValueError, e:
-          self.fail("Unexpected failure: %s" % e)
-      
-      #failures
-      for x in [1.2, "bad", {"test":"blah"}, [32, 49], None, True]:
-        try:
-          jsonschema.validate(x, {"type":"integer"})
-        except ValueError:
-          pass
-        else:
-          self.fail("Expected failure for %s" % repr(x))
-    
+        valid_ints = [1, -89, 420000]
+        invalid_ints = [1.2, "bad", {"test":"blah"}, [32, 49], None, True]
+        self._test_type('integer', valid_ints, invalid_ints)
+
     def test_string(self):
-      for x in ["surrender?", "nuts!", "ok", "@hsuha", "\'ok?\'", "blah"]:
-        try:
-          jsonschema.validate(x, {"type":"string"})
-        except ValueError, e:
-          self.fail("Unexpected failure: %s" % e)
-      
-      #failures
-      for x in [1.2, 1, {"test":"blah"}, [32, 49], None, True]:
-        try:
-          jsonschema.validate(x, {"type":"string"})
-        except ValueError:
-          pass
-        else:
-          self.fail("Expected failure for %s" % repr(x))
-    
+        valids = ["abc", u"unicode"]
+        invalids = [1.2, 1, {"test":"blah"}, [32, 49], None, True]
+        self._test_type('string', valids, invalids)
+
     def test_number(self):
-      for x in [1.2, 89.42, 48.5224242, 32, 49, 42.24324]:
-        try:
-          jsonschema.validate(x, {"type":"number"})
-        except ValueError, e:
-          self.fail("Unexpected failure: %s" % e)
-      
-      #failures
-      for x in ["bad", {"test":"blah"}, [32.42, 494242], None, True]:
-        try:
-          jsonschema.validate(x, {"type":"number"})
-        except ValueError:
-          pass
-        else:
-          self.fail("Expected failure for %s" % repr(x))
-    
+        valids = [1.2, -89.42, 48, -32]
+        invalids = ["bad", {"test":"blah"}, [32.42, 494242], None, True]
+        self._test_type('number', valids, invalids)
+
     def test_boolean(self):
-      for x in [True, False]:
-        try:
-          jsonschema.validate(x, {"type":"boolean"})
-        except ValueError, e:
-          self.fail("Unexpected failure: %s" % e)
-      
-      #failures
-      for x in [1.2, "False", {"test":"blah"}, [32, 49], None, 1, 0]:
-        try:
-          jsonschema.validate(x, {"type":"boolean"})
-        except ValueError:
-          pass
-        else:
-          self.fail("Expected failure for %s" % repr(x))
-    
+        valids = [True, False]
+        invalids = [1.2, "False", {"test":"blah"}, [32, 49], None, 1, 0]
+        self._test_type('boolean', valids, invalids)
+
     def test_object(self):
-      for x in [{"blah": "test"}, {"this":{"blah":"test"}}, {1:2, 10:20}]:
-        try:
-          jsonschema.validate(x, {"type":"object"})
-        except ValueError, e:
-          self.fail("Unexpected failure: %s" % e)
-      
-      #failures
-      for x in [1.2, "bad", 123, [32, 49], None, True]:
-        try:
-          jsonschema.validate(x, {"type":"object"})
-        except ValueError:
-          pass
-        else:
-          self.fail("Expected failure for %s" % repr(x))
-    
+        valids = [{"blah": "test"}, {"this":{"blah":"test"}}, {1:2, 10:20}]
+        invalids = [1.2, "bad", 123, [32, 49], None, True]
+        self._test_type('object', valids, invalids)
+
     def test_array(self):
-      for x in [[1, 89], [48, {"test":"blah"}, "49", 42]]:
-        try:
-          jsonschema.validate(x, {"type":"array"})
-        except ValueError, e:
-          self.fail("Unexpected failure: %s" % e)
-      
-      #failures
-      for x in [1.2, "bad", {"test":"blah"}, 1234, None, True]:
-        try:
-          jsonschema.validate(x, {"type":"array"})
-        except ValueError:
-          pass
-        else:
-          self.fail("Expected failure for %s" % repr(x))
-    
+        valids = [[1, 89], [48, {"test":"blah"}, "49", 42]]
+        invalids = [1.2, "bad", {"test":"blah"}, 1234, None, True]
+        self._test_type('array', valids, invalids)
+
     def test_null(self):
-      
-      try:
-        jsonschema.validate(None, {"type":"null"})
-      except ValueError, e:
-        self.fail("Unexpected failure: %s" % e)
-      
-      #failures
-      for x in [1.2, "bad", {"test":"blah"}, [32, 49], 1284, True]:
-        try:
-          jsonschema.validate(x, {"type":"null"})
-        except ValueError:
-          pass
-        else:
-          self.fail("Expected failure for %s" % repr(x))
-    
+        valids = [None]
+        invalids = [1.2, "bad", {"test":"blah"}, [32, 49], 1284, True]
+        self._test_type('null', valids, invalids)
+
     def test_any(self):
-      
-      #test "any" and default value
-      for y in [{"type": "any"}, {}]:
-        for x in [1.2, "bad", {"test":"blah"}, [32, 49], None, 1284, True]:
-          try:
-            jsonschema.validate(x, y)
-          except ValueError:
-            self.fail("Unexpected failure: %s" % e)
+        valids = [1.2, "bad", {"test":"blah"}, [32, 49], None, 1284, True]
+        self._test_type('any', valids, [])
+
+    def test_default(self):
+        # test default value (same as any really)
+        valids = [1.2, "bad", {"test":"blah"}, [32, 49], None, 1284, True]
+        for x in valids:
+            try:
+                jsonschema.validate(x, {})
+            except ValueError:
+                self.fail("Unexpected failure: %s" % e)
+
+    def test_multi(self):
+        types = ["null", "integer", "string"]
+        valids = [None, 42, "string"]
+        invalids = [1.2, {"test":"blah"}, [32, 49], True]
+        self._test_type(types, valids, invalids)
+
+
+class TestDisallow(TestType):
+    def _test_type(self, typename, valids, invalids):
+        for x in invalids:
+            try:
+                jsonschema.validate(x, {"disallow":typename})
+            except ValueError, e:
+                self.fail("Unexpected failure: %s" % e)
+
+        for x in valids:
+            self.assertRaises(ValueError, jsonschema.validate, x,
+                              {"disallow":typename})
+
