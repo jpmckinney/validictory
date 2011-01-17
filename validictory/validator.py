24b82958721117028a76793564057c7cf5fe2b6c
Filip Noetzel <filip@j03.de>
2011-01-17 09:05:25 +0100

    Be more specific about {"uniqueItems": false}

--- validictory/validator.py
+++ validictory/validator.py
@@ -310,14 +310,16 @@ class SchemaValidator(object):
                 self._error("Value %(value)r for field '%(fieldname)s' does not match regular expression '%(pattern)s'",
                             value, fieldname, pattern=pattern)
 
-    def validate_uniqueItems(self, x, fieldname, schema, unique=False):
+    def validate_uniqueItems(self, x, fieldname, schema, uniqueItems=False):
         '''
         Validates that all items in an array instance MUST be unique
         (contains no two identical values).
         '''
 
-        if not unique:
-            return # nothing to validate
+        # If additionalProperties is the boolean value True then we accept
+        # any additional properties.
+        if isinstance(uniqueItems, bool) and not uniqueItems:
+            return
 
         values = x.get(fieldname)
 
