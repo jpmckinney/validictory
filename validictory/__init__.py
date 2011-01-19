19397213ba8083ddf62e147ca473059209131980
James Turk <james.p.turk@gmail.com>
2011-01-19 11:58:22 -0500

    add required_by_default to validate call

--- validictory/__init__.py
+++ validictory/__init__.py
@@ -5,7 +5,8 @@ from validictory.validator import SchemaValidator, ValidationError
 __all__ = [ 'validate', 'SchemaValidator', 'ValidationError' ]
 __version__ = '0.6.0-dev'
 
-def validate(data, schema, validator_cls=SchemaValidator, format_validators=None):
+def validate(data, schema, validator_cls=SchemaValidator,
+             format_validators=None, required_by_default=True):
     '''
     Validates a parsed json document against the provided schema. If an
     error is found a ValueError is raised.
@@ -20,8 +21,11 @@ def validate(data, schema, validator_cls=SchemaValidator, format_validators=None
 
     If ``format_validators`` is provided, the validator_cls will be
     instanciated with these default format validators.
+
+    ``required_by_default`` specifies if ``required`` should default to True
+    or False (default: True)
     '''
-    v = validator_cls(format_validators)
+    v = validator_cls(format_validators, required_by_default)
     return v.validate(data,schema)
 
 if __name__ == '__main__':
