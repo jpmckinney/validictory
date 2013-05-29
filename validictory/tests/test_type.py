76aeb089efdd93d9c880389eac5316a7c43ce509
Marc Abramowitz <marc@marc-abramowitz.com>
2013-05-29 11:22:21 -0700

    Test that webob.multidict.NestedMultiDict and sqlalchemy.engine.result.RowProxy dict-like objects are treated like dicts (as {"type": "object"})

--- validictory/tests/test_type.py
+++ validictory/tests/test_type.py
@@ -79,6 +79,20 @@ class TestType(TestCase):
         invalids = []
         self._test_type('object', valids, invalids)
 
+    def test_sqlalchemy_RowProxy_is_object(self):
+        import sqlalchemy
+        engine = sqlalchemy.create_engine('sqlite://')
+        row = engine.execute('SELECT 1').fetchone()
+        valids = [row]
+        invalids = []
+        self._test_type('object', valids, invalids)
+
+    def test_webob_multidict_NestedMultiDict_is_object(self):
+        import webob
+        valids = [webob.multidict.NestedMultiDict({'a': 'b'})]
+        invalids = []
+        self._test_type('object', valids, invalids)
+
     def test_object(self):
         valids = [{"blah": "test"}, {"this": {"blah": "test"}}, {1: 2, 10: 20}]
         invalids = [1.2, "bad", 123, [32, 49], None, True]
