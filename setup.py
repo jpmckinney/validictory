2fa96845b62cdd049d5c813323dce79ac8da2a16
James McKinney <26463+jpmckinney@users.noreply.github.com>
2023-12-21 15:47:00 -0500

    build(setup.py): Exclude tests from installed packages. Fix license classifier. Update version classifiers. Add test extras. Remove unnecessary options.

--- setup.py
+++ setup.py
@@ -1,30 +1,39 @@
-#!/usr/bin/env python
-
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 from validictory import __version__
-DESCRIPTION = "general purpose python data validator"
-LONG_DESCRIPTION = open('README.rst').read()
+
+with open('README.rst') as f:
+    long_description = f.read()
 
 setup(name='validictory',
       version=__version__,
-      description=DESCRIPTION,
-      long_description=LONG_DESCRIPTION,
+      description='general purpose python data validator',
+      long_description=long_description,
+      long_description_content_type='text/x-rst',
       author='James Turk',
       author_email='james.p.turk@gmail.com',
       url='http://github.com/jamesturk/validictory',
-      license="MIT License",
-      platforms=["any"],
-      packages=find_packages(),
-      test_suite="validictory.tests",
+      license='MIT',
       classifiers=["Development Status :: 4 - Beta",
                    "Intended Audience :: Developers",
-                   "License :: OSI Approved :: BSD License",
+                   "License :: OSI Approved :: MIT License",
                    "Natural Language :: English",
                    "Operating System :: OS Independent",
-                   "Programming Language :: Python :: 2.7",
-                   "Programming Language :: Python :: 3.4",
-                   "Programming Language :: Python :: 3.5",
+                   'Programming Language :: Python',
+                   'Programming Language :: Python :: 3.8',
+                   'Programming Language :: Python :: 3.9',
+                   'Programming Language :: Python :: 3.10',
+                   'Programming Language :: Python :: 3.11',
+                   'Programming Language :: Python :: 3.12',
+                   'Programming Language :: Python :: Implementation :: CPython',
+                   'Programming Language :: Python :: Implementation :: PyPy',
                    "Topic :: Software Development :: Libraries :: Python Modules",
                    ],
-     )
+      packages=find_packages(exclude=['validictory.tests', 'validictory.tests.*']),
+      extras_require={
+          'test': [
+              'pytest',
+              'pytest-cov',
+          ]
+      }
+)
